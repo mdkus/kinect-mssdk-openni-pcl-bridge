e16cc726f0c516f455cd73f2ed5283adeb588c32
tomoto335 <tomoto@gmail.com>
2011-06-22 20:02:08 -0700

    Add installer batch files

---
+++ install.bat
@@ -0,0 +1,3 @@
+@echo Note: this script must be run as administrator.
+"%OPEN_NI_BIN%\niReg"  %0\..\bin\Release\kinect-mssdk-openni-bridge.dll && @echo OK
+pause
---
+++ uninstall.bat
@@ -0,0 +1,3 @@
+@echo Note: this script must be run as administrator.
+"%OPEN_NI_BIN%\niReg" -u %0\..\bin\Release\kinect-mssdk-openni-bridge.dll && @echo OK
+pause
