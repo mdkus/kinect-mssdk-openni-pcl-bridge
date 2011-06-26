2fb914d551fb248768713abefe0fd9e1e1947b12
tomoto335 <tomoto@gmail.com>
2011-06-26 12:25:18 -0700

    Fixed documents and integrated uninstaller.

--- uninstall-debug.bat
+++
@@ -1,3 +0,0 @@
-@echo Note: this script must be run as administrator.
-"%OPEN_NI_BIN%\niReg" -u %0\..\bin\Debug\kinect-mssdk-openni-bridge.dll && @echo OK
-pause
--- uninstall.bat
+++ uninstall.bat
@@ -1,3 +1,4 @@
 @echo Note: this script must be run as administrator.
 "%OPEN_NI_BIN%\niReg" -u %0\..\bin\Release\kinect-mssdk-openni-bridge.dll && @echo OK
+"%OPEN_NI_BIN%\niReg" -u %0\..\bin\Debug\kinect-mssdk-openni-bridge.dll && @echo OK
 pause
