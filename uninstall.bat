011fb3542aac9e73b531a1458d911b5378a7577e
tomoto335 <tomoto@gmail.com>
2011-11-06 05:03:56 -0800

    Beta1/Beta2 multi installer

--- uninstall.bat
+++ uninstall.bat
@@ -1,13 +1,19 @@
-@echo ----------
 @setlocal
 @IF "%OPEN_NI_BIN64%" == "" (SET NIREG_PATH="%OPEN_NI_BIN%\niReg") ELSE (SET NIREG_PATH="%OPEN_NI_BIN64%\niReg64")
-%NIREG_PATH% -u %0\..\bin\Release\kinect-mssdk-openni-bridge.dll
+@echo ----------
+%NIREG_PATH% -u %0\..\bin\kinect-mssdk-openni-bridge-Beta1Release.dll
 @IF NOT "%ERRORLEVEL%" == "0" GOTO ERR
 @echo OK!
 @echo ----------
-@setlocal
-@IF "%OPEN_NI_BIN64%" == "" (SET NIREG_PATH="%OPEN_NI_BIN%\niReg") ELSE (SET NIREG_PATH="%OPEN_NI_BIN64%\niReg64")
-%NIREG_PATH% -u %0\..\bin\Debug\kinect-mssdk-openni-bridge.dll
+%NIREG_PATH% -u %0\..\bin\kinect-mssdk-openni-bridge-Beta1Debug.dll
+@IF NOT "%ERRORLEVEL%" == "0" GOTO ERR
+@echo OK!
+@echo ----------
+%NIREG_PATH% -u %0\..\bin\kinect-mssdk-openni-bridge-Beta2Release.dll
+@IF NOT "%ERRORLEVEL%" == "0" GOTO ERR
+@echo OK!
+@echo ----------
+%NIREG_PATH% -u %0\..\bin\kinect-mssdk-openni-bridge-Beta2Debug.dll
 @IF NOT "%ERRORLEVEL%" == "0" GOTO ERR
 @echo OK!
 @GOTO END
